504002a1daa709c0d404f2a8face57dde83e3f4d
nociza <azicon@berkeley.edu>
2023-03-08 00:54:11 -0800

    simple bing chat interface

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import { BingChat } from "bing-chat";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello from Bob!");
+});
+
+router.post("/", async (req, res) => {
+  const { message } = req.body;
+  const api = new BingChat({
+    cookie: process.env.BING_COOKIE,
+  });
+  const response = await api.sendMessage(message);
+  res.send(response.text);
 });
 
 export default router;
