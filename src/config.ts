8862e36fd0cf4431a93914597e4b0d7bfc2eafb2
nociza <azicon@berkeley.edu>
2023-03-08 00:44:26 -0800

    bunch o boilerplate

--- src/config.ts
+++ src/config.ts
@@ -4,19 +4,11 @@ import "dotenv/config";
 interface Config {
   /** The port that the express server should bind to. */
   port: number;
-  /** The secret key used to sign JWTs. */
-  jwtPrivateKey: string;
-  emailPrivateKey: string;
-  accessToken: string;
   mongoDbUri: string;
 }
 
 const config: Config = {
   port: nodeConfig.get<number>("port"),
-  jwtPrivateKey:
-    process.env.JWT_PRIVATE_KEY || nodeConfig.get<string>("jwtPrivateKey"),
-  emailPrivateKey: process.env.EMAIL_PRIV_KEY,
-  accessToken: process.env.ACCESS_TOKEN,
   mongoDbUri: nodeConfig.get<string>("db"),
 };
 
